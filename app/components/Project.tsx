450c44323fa01c3585605807db2e23d57eb58da9
Oliver Eriksen <okebedrift@gmail.com>
2024-05-29 14:00:06 +0200

    project modal backup

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Olink from "./Olink";
 import Image from "next/image";
 import Link from "next/link";
+import ModalImage from "react-modal-image";
 
 interface Props {
   projectName: string;
@@ -12,6 +13,15 @@ interface Props {
 
 const Project = ({ projectName, para, imageAmount }: Props) => {
   const [paraState, setParaState] = React.useState(false);
+  const [clickedImage, setClickedImage] = useState<string | null>(null);
+
+  const handleImageClick = (src: string) => {
+    setClickedImage(src);
+  };
+
+  const closePreview = () => {
+    setClickedImage(null);
+  };
 
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
@@ -20,13 +30,13 @@ const Project = ({ projectName, para, imageAmount }: Props) => {
 
   return (
     <>
-      <div className="px-4 w-full mt-20 transition">
+      <div className="px-4 w-full mt-20 z-20 transition">
         <h3 className="w-full text-center">{projectName}</h3>
         <div className="title text-center flex flex-col flex-wrap content-center">
           <div className="separate-line w-52 mt-2"></div>
         </div>
 
-        <div className={`2xl:w- col1 flex justify-center`}>
+        <div className={`2xl:w- col1 flex justify-center pt-4`}>
           {/* {paraState === true ? (
           <p className="lg:w-96 md:w-86 p-5 transition ">{para}</p>
         ) : (
@@ -37,8 +47,8 @@ const Project = ({ projectName, para, imageAmount }: Props) => {
               paraState === true ? "w-full" : "w-680"
             }   pr-12 flex flex-col h-80 transition `}
           >
-            <div className={`project-header flex  mt-4 h-80`}>
-              <div className="flex flex-col z-20 gray1-bg">
+            <div className={`project-header flex relative h-80`}>
+              <div className="flex flex-col z-30 gray1-bg">
                 {paraState === true ? (
                   <p className="lg:w-96 md:w-86 p-5 transition ">{para}</p>
                 ) : (
@@ -62,18 +72,22 @@ const Project = ({ projectName, para, imageAmount }: Props) => {
                   </button>
                 </div>
               </div>
-              <div className="flex  relative z-20 gray1-bg">
+              <div className="flex relative z-30 gray1-bg">
                 <Image
                   width={200}
                   height={300}
                   src={`${projectName}/forside.png`}
                   alt="Magazine cover"
-                  className="h-full w-auto mx-2"
+                  className={`h-full w-auto mx-2 ${
+                    clickedImage === `${projectName}/forside.png`
+                      ? "fixed top-0 left-0 object-cover z-50 mx-auto"
+                      : ""
+                  }`}
+                  onClick={() => handleImageClick(`${projectName}/forside.png`)}
                 />
               </div>
-
               <div
-                className={`max-w-none flex h-80 w-fit absolute z-19 transition-all duration-1000 ease-in-out transform ${
+                className={`max-w-none flex h-80 w-fit absolute transition-all duration-1000 ease-in-out transform ${
                   paraState == true
                     ? "ml-full translate-x-0 opacity-100 right-0"
                     : "ml-full project-translate opacity-100 right-0"
@@ -86,20 +100,37 @@ const Project = ({ projectName, para, imageAmount }: Props) => {
                     key={i}
                     src={`${projectName}/${i + 1}.jpg`}
                     alt={`Image ${i + 1}`}
-                    className={` mx-5 w-auto h-full max-w-none transition-all duration-1000 ease-in-out
-                    ${paraState == true ? "opacity-100" : "opacity-0"}`}
+                    className={`mx-1 w-auto h-full max-w-none transition-all duration-1000 ease-in-out transform ${
+                      paraState == true ? "opacity-100" : "opacity-0"
+                    } ${
+                      clickedImage === `${projectName}/${i + 1}.jpg`
+                        ? "fixed top-0 left-0 object-cover z-50"
+                        : ""
+                    }`}
+                    onClick={() =>
+                      handleImageClick(`${projectName}/${i + 1}.jpg`)
+                    }
                   />
                 ))}
               </div>
             </div>
           </div>
         </div>
-        {/* <div
-        className={` ${paraState == true ? "w-80" : "w-full"}  p-5 transition `}
-      >
-        {para}
-      </div> */}
       </div>
+      {clickedImage && (
+        <div
+          className="fixed top-0 left-0 z-100 flex items-center justify-center"
+          onClick={closePreview}
+        >
+          <Image
+            src={clickedImage}
+            alt="Clicked image"
+            layout="fill"
+            objectFit="contain"
+            className="z-70"
+          />
+        </div>
+      )}
     </>
   );
 };
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -3,7 +3,15 @@ import Title from "./Title";
 import Project from "./Project";
 
 const Projects = () => {
-  const projects = ["Magazine", "kampanje", "intervju", "karm", "karm", "karm"];
+  const projects = [
+    "Magazine",
+    "kampanje",
+    "intervju",
+    "karm",
+    "karm",
+    "karm",
+    "nettside",
+  ];
   const paras = [
     "A magazine website that I developed for a school project. The website is made with React and TailwindCSS.",
     "A campaign website that I developed for a school project. The website is made with React and TailwindCSS.",
@@ -11,8 +19,9 @@ const Projects = () => {
     "A website that I developed for a school project. The website is made with React and TailwindCSS.",
     "A website that I developed for a school project. The website is made with React and TailwindCSS.",
     "A website that I developed for a school project. The website is made with React and TailwindCSS.",
+    "hei på deg 123",
   ];
-  const imageAmounts = [13, 1, 3, 3, 3, 3];
+  const imageAmounts = [13, 1, 3, 3, 3, 3, 8];
 
   return (
     <>
